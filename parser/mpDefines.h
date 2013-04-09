34495486c29d7cfb64149fcc8ee2e1424513c2d1
rotter.martinos <rotter.martinos@36614c78-fac1-fd0a-2fe7-d81eeab653c6>
2013-04-09 07:01:05 +0000

    Version updated.

--- parser/mpDefines.h
+++ parser/mpDefines.h
@@ -62,7 +62,7 @@
 #endif
 
 /** \brief A macro containing the version of muParserX. */
-#define MUP_PARSER_VERSION _T("2.1.7 (20130303; dev)")
+#define MUP_PARSER_VERSION _T("2.1.7 (20130409; dev)")
 
 /** \brief A macro for setting the parser namespace. */
 #define MUP_NAMESPACE_START namespace mup {
