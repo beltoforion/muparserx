2758a98fc46379137de732be8ed2fca4b50bc9d7
iberg@gmx.info <iberg@gmx.info@36614c78-fac1-fd0a-2fe7-d81eeab653c6>
2014-11-24 00:08:19 +0000

    Fix for Issue 41; Compilation problems on VS 2013 solved

--- trunk/parser/mpDefines.h
+++ trunk/parser/mpDefines.h
@@ -62,7 +62,7 @@
 #endif
 
 /** \brief A macro containing the version of muParserX. */
-#define MUP_PARSER_VERSION _T("3.0.3 (20141011; release)")
+#define MUP_PARSER_VERSION _T("3.0.4 (20141124; development snapshot)")
 
 /** \brief A macro for setting the parser namespace. */
 #define MUP_NAMESPACE_START namespace mup {
